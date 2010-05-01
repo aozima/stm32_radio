4292cb96871f095550db442898bf178227278ea7
bernard.xiong <bernard.xiong@e226a92b-005e-15d2-9e4b-6c8d4ba7dc2d>
2010-05-01 13:23:26 +0000

    add led simple example.
    git-svn-id: http://stm32radio.googlecode.com/svn@10 e226a92b-005e-15d2-9e4b-6c8d4ba7dc2d

---
+++ trunk/examples/0_led_simple/application.c
@@ -0,0 +1,39 @@
+#include <rtthread.h>
+#include "led.h"
+
+static struct rt_thread led_thread;
+static rt_uint32_t led_stack[512/4];
+static void led_thread_entry(void* parameter)
+{
+    unsigned int count=0;
+    while (1)
+    {
+        /* led1 on */
+        rt_kprintf("led on, count : %d\r\n",count);
+        count++;
+        rt_hw_led_on(0);
+        rt_thread_delay(50); /* sleep 0.5 second and switch to other thread */
+
+        /* led1 off */
+        rt_kprintf("led2 off\r\n");
+        rt_hw_led_off(0);
+        rt_thread_delay(50);
+    }
+}
+
+int rt_application_init()
+{
+	rt_err_t result;
+
+    /* init led thread */
+	result = rt_thread_init(&led_thread, 
+		"led",
+		led_thread_entry, RT_NULL, 
+		(rt_uint8_t*)&led_stack[0], sizeof(led_stack), 20, 5);
+	if (result == RT_EOK)
+	{
+        rt_thread_startup(&led_thread);
+	}
+
+    return 0;
+}
