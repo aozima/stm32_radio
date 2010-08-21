7575541af06c33c69fbef70fe8f877625f91b20d
bernard.xiong <bernard.xiong@e226a92b-005e-15d2-9e4b-6c8d4ba7dc2d>
2010-08-21 07:07:39 +0000

    add a label on the view.
    git-svn-id: http://stm32radio.googlecode.com/svn@46 e226a92b-005e-15d2-9e4b-6c8d4ba7dc2d

--- trunk/examples/4_gui_single_panel/single_panel.c
+++ trunk/examples/4_gui_single_panel/single_panel.c
@@ -1,6 +1,7 @@
 #include <rtthread.h>
 #include <rtgui/rtgui_server.h>
 #include <rtgui/rtgui_system.h>
+#include <rtgui/widgets/label.h>
 #include <rtgui/widgets/workbench.h>
 
 #include "lcd.h"
@@ -11,7 +12,9 @@ void workbench_panel1(void* parameter)
 {
 	rt_mq_t mq;
 	rtgui_view_t* view;
+	rtgui_label_t* label;
 	struct rtgui_workbench* workbench;
+	rtgui_rect_t rect;
 
 	mq = rt_mq_create("wmq", 256, 8, RT_IPC_FLAG_FIFO);
 	/* 注册当前线程为GUI线程 */
@@ -24,6 +27,14 @@ void workbench_panel1(void* parameter)
 	if (view == RT_NULL) return;
 	/* 指定视图的背景色 */
 	RTGUI_WIDGET_BACKGROUND(RTGUI_WIDGET(view)) = white;
+
+	/* 添加一个label */
+	label = rtgui_label_create("你好！RT-Thread！");
+	rect.x1 = 10; rect.y1 = 10;
+	rect.x2 = 210; rect.y2 = 30;
+	/* 设置label的位置 */
+	rtgui_widget_set_rect(RTGUI_WIDGET(label), &rect);
+	rtgui_container_add_child(RTGUI_CONTAINER(view), RTGUI_WIDGET(label));
 
 	/* 添加到父workbench中 */
 	rtgui_workbench_add_view(workbench, view);
