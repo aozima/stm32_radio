fdb0c3ef7b483ccefbb8d7025c00ce8d6d749729
wuyangyong <wuyangyong@e226a92b-005e-15d2-9e4b-6c8d4ba7dc2d>
2011-06-26 13:45:26 +0000

    update project file
    git-svn-id: http://stm32radio.googlecode.com/svn@129 e226a92b-005e-15d2-9e4b-6c8d4ba7dc2d

--- trunk/stm32radio_examples/examples/6_ext_sram/stm32f10x_it.c
+++ trunk/stm32radio_examples/examples/6_ext_sram/stm32f10x_it.c
@@ -45,19 +45,6 @@ void NMI_Handler(void)
 }
 
 /**
-  * @brief  This function handles Hard Fault exception.
-  * @param  None
-  * @retval None
-  */
-void HardFault_Handler(void)
-{
-    /* Go to infinite loop when Hard Fault exception occurs */
-    while (1)
-    {
-    }
-}
-
-/**
   * @brief  This function handles Memory Manage exception.
   * @param  None
   * @retval None
@@ -113,6 +100,13 @@ void SVC_Handler(void)
 void DebugMon_Handler(void)
 {
 }
+
+void SysTick_Handler(void)
+{
+    extern void rt_hw_timer_handler(void);
+    rt_hw_timer_handler();
+}
+
 
 /******************************************************************************/
 /*                 STM32F10x Peripherals Interrupt Handlers                   */
