d6c4696a0f10efa9f3ca6ecb157c54c39d65c091
wuyangyong <wuyangyong@e226a92b-005e-15d2-9e4b-6c8d4ba7dc2d>
2011-06-26 13:56:40 +0000

    update project
    git-svn-id: http://stm32radio.googlecode.com/svn@130 e226a92b-005e-15d2-9e4b-6c8d4ba7dc2d

--- trunk/stm32radio_examples/examples/0_led_simple/application.c
+++ trunk/stm32radio_examples/examples/0_led_simple/application.c
@@ -9,15 +9,19 @@ static void led_thread_entry(void* parameter)
     while (1)
     {
         /* led1 on */
+#ifndef RT_USING_FINSH
         rt_kprintf("led on, count : %d\r\n",count);
+#endif
         count++;
         rt_hw_led_on(0);
-        rt_thread_delay(50); /* sleep 0.5 second and switch to other thread */
+        rt_thread_delay( RT_TICK_PER_SECOND/2 ); /* sleep 0.5 second and switch to other thread */
 
         /* led1 off */
+#ifndef RT_USING_FINSH
         rt_kprintf("led2 off\r\n");
+#endif
         rt_hw_led_off(0);
-        rt_thread_delay(50);
+        rt_thread_delay( RT_TICK_PER_SECOND/2 );
     }
 }
 
@@ -26,9 +30,9 @@ int rt_application_init()
 	rt_err_t result;
 
     /* init led thread */
-	result = rt_thread_init(&led_thread, 
+	result = rt_thread_init(&led_thread,
 		"led",
-		led_thread_entry, RT_NULL, 
+		led_thread_entry, RT_NULL,
 		(rt_uint8_t*)&led_stack[0], sizeof(led_stack), 20, 5);
 	if (result == RT_EOK)
 	{
